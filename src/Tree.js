7eb509aee01daff38a4891cb9f59fed8cdea109b
Tommy <tommy128works@gmail.com>
2023-07-20 10:34:16 -0400

    feat: add sort array and delete duplicate values

--- src/Tree.js
+++ src/Tree.js
@@ -6,6 +6,16 @@ const Tree = (array) => {
   const buildTree = (array) => {
 
     array.sort(function(a, b) {return a - b});
+    let index = 1;
+    let previousValue = array[0];
+    while(array[index]) {
+      if (array[index] === previousValue) {
+        array.splice(index, 1);
+      } else {
+        previousValue = array[index];
+        index++;
+      }
+    }
 
     console.log(array);
 
